e8e1c8fc44d2df5d46ae90ed446f823454ba3351
Shreyas Mohanty <shreyasm1905@gmail.com>
2026-07-02 08:43:21 +0530

    Update init for services

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -1,12 +1,12 @@
 # app/services/__init__.py
-#from .graph_engine import MultiCloudGraphEngine
-# from .ml_inference import ParallelMLEngine
-# from .risk_engine import HawkesRiskEngine
-# from .xai_engine import ShaXaiEngine
+from .graph_engine import MultiCloudGraphEngine
+from .ml_inference import ParallelMLEngine
+from .risk_engine import HawkesRiskEngine
+from .xai_triage import generate_soc_narrative
 
-# __all__ = [
-#     #"MultiCloudGraphEngine",
-#     # "ParallelMLEngine",
-#     # "HawkesRiskEngine",
-#     # "ShaXaiEngine"
-# ]
+__all__ = [
+    "MultiCloudGraphEngine",
+    "ParallelMLEngine",
+    "HawkesRiskEngine",
+    "generate_soc_narrative"
+]
