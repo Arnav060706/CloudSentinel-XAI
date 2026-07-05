07707ad5d3223469e2dfdead81437f6ff2d8da4f
Shreyas Mohanty <shreyasm1905@gmail.com>
2026-07-06 01:19:07 +0530

    Edited hawk risk engine to risk engine

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -1,12 +1,9 @@
-# app/services/__init__.py
 from .graph_engine import MultiCloudGraphEngine
 from .ml_inference import ParallelMLEngine
-from .risk_engine import HawkesRiskEngine
-from .xai_triage import generate_soc_narrative
+from .risk_engine import RiskEngine
 
 __all__ = [
     "MultiCloudGraphEngine",
     "ParallelMLEngine",
-    "HawkesRiskEngine",
-    "generate_soc_narrative"
+    "RiskEngine",
 ]
